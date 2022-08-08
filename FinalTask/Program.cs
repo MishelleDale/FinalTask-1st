c0f24842556656e1fca6f814756027ad8ef49dc8
Ekaterina <mishelledale@gmail.com>
2022-08-08 18:38:35 +0300

    The Final task is done. Watch README to know more about the task

---
+++ FinalTask/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Сколько элементов вы хотите ввести?");
+int Т = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[Т];
+string[] newArray = new string[array.Length];
+for (int i = 0; i < Т; i++)
+{
+    Console.Write($"Введите {i + 1} элемент: "); // to culculate the order in user's way 1, 2, 3...
+    array[i] = Console.ReadLine();
+}
+void SelectString(string[] array, string[] newArray)
+{
+    int number = 0;
+    int length = 4; // 3+1 to build the strict border
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < length)
+        {
+            newArray[number] = array[i];
+            number++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Исходный массив: ");
+PrintArray(array);
+SelectString(array, newArray);
+Console.WriteLine("Полученный массив: ");
+PrintArray(newArray);
+
